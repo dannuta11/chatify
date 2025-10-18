b395a6c9e9e12c68282719964cc10e46a1c7809e
dannuta11 <dan.netbeau@yahoo.com>
2025-10-19 02:01:12 +0300

    fix: restart the server when a change is made

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend", "dist")));
