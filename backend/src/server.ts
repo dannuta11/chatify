e03f628aa8ee4c96414d80b0e7143f7b0615c1eb
dannuta11 <dan.netbeau@yahoo.com>
2025-11-02 23:09:29 +0200

    feat: Install prisma (#2)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,8 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend", "dist")));
-
-app.get("/", (_, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
