46e7cf48edcfe0866973b63ec4efa56453d78f6e
dannuta11 <dan.netbeau@yahoo.com>
2025-11-24 17:43:50 +0200

    feat: config eslint

---
+++ backend/eslint.config.ts
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "prefer-const": "error",
+      "import/order": [
+        1,
+        {
+          groups: [
+            "external",
+            "builtin",
+            "internal",
+            "sibling",
+            "parent",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+]);
