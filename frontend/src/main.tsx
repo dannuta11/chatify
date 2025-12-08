3ebe29ef8f534cd20b9d75906c1196227674a5aa
dannuta11 <dan.netbeau@yahoo.com>
2025-12-08 02:08:34 +0200

    feat: add home path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ConfigProvider } from "antd";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 
-const queryClient = new QueryClient();
+import { router } from "@routes/index";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </ConfigProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
